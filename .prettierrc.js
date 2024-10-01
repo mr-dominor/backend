2d09397c453d164875da1a6eacc870719c717c15
mubashshir <mubashshirislam201@gmail.com>
2024-10-01 09:33:13 +0530

    initial files part2

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.export ={
+  singleQuote: true,      // Use single quotes
+  trailingComma: 'none',  // No trailing commas
+  tabWidth: 2,            // Indentation width is 2 spaces
+  printWidth: 80          // Line width for wrapping is 80 characters
+};
+
+"Remember to push changed file on github"
